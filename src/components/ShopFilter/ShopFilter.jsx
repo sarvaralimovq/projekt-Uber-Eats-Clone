162b29bd9ae212fa4e4a19e7be7b8eeb283be1f0
sarvaralimovq <sarvaralimovq@gmail.com>
2024-01-26 22:15:29 +0500

    salom Ziyodilo

--- src/components/ShopFilter/ShopFilter.jsx
+++ src/components/ShopFilter/ShopFilter.jsx
@@ -6,7 +6,13 @@ function ShopFilter() {
         <div className="container">
             <div className="shopFilter__inner">
                 <div className="shopFilter__filter">
-                  
+                  <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                  </ul>
                 </div>
             </div>
         </div>

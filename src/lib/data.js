24f26d403977214d48fec1cdb41b27cdd0540d41
ziyo2007 <qodirjonovz6@gmail.com>
2024-01-26 21:36:58 +0300

    sdf

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,3 +1,12 @@
+import product1 from '../img/product-1.png'
+import product2 from '../img/product-2.png'
+import product3 from '../img/product-3.png'
+import product4 from '../img/product-4.png'
+import product5 from '../img/product-5.png'
+import product6 from '../img/product-6.png'
+import product7 from '../img/product-7.png'
+import product8 from '../img/product-8.png'
+
 export const shopData = [
     {
         id:1,
@@ -9,8 +18,74 @@ export const shopData = [
 export const homeProdukt =[
     {
         id:1,
-        img:'',
-        name:'',
-            
+        img:product1,
+        name:'Syltherine',
+        info:'Stylish cafe chair',
+        price:'2.500.000',
+        oldprice:'3.500.000',
+        chegirma:'-30%',
     },
+    // {
+    //     id:2,
+    //     img:product2,
+    //     name:'Leviosa',
+    //     info:'Stylish cafe chair',
+    //     price:'2.500.000',
+    //     oldprice:'',
+    //     chegirma:'',
+    // },
+    // {
+    //     id:3,
+    //     img:product3,
+    //     name:'Lolito',
+    //     info:'Luxury big sofa',
+    //     price:'7.000.000',
+    //     oldprice:'14.000.000',
+    //     chegirma:'-50',
+    // },
+    // {
+    //     id:4,
+    //     img:product4,
+    //     name:'Respira',
+    //     info:'Outdoor bar table and stool',
+    //     price:'500.000',
+    //     oldprice:'',
+    //     chegirma:'New',
+    // },
+    // {
+    //     id:5,
+    //     img:product5,
+    //     name:'Grifo',
+    //     info:'Night lamp',
+    //     price:'1.500.000',
+    //     oldprice:'',
+    //     chegirma:'',
+    // },
+    // {
+    //     id:6,
+    //     img:product6,
+    //     name:'Muggo',
+    //     info:'Small mug',
+    //     price:'150.000',
+    //     oldprice:'',
+    //     chegirma:'New',
+    // },
+    // {
+    //     id:7,
+    //     img:product7,
+    //     name:'Pingky',
+    //     info:'Cute bed set',
+    //     price:'7.000.000',
+    //     oldprice:'14.000.000',
+    //     chegirma:'-50',
+    // },
+    // {
+    //     id:8,
+    //     img:product8,
+    //     name:'Potty',
+    //     info:'Minimalist flower pot',
+    //     price:'500.000',
+    //     oldprice:'',
+    //     chegirma:'New',
+    // },
 ]
--- src/page/Home/Products/Products.jsx
+++ src/page/Home/Products/Products.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import './Products.scss'
+import { homeProdukt } from '../../../lib/data'
 
 function Products() {
   return (
     <div className='Products'>
-        <h1>Products</h1>
+       <div className="container">
+       <h1 className='products__tittle'>Our Products</h1>
+        <ul className='products__list'>
+          {
+            homeProdukt?.map((item,index)=> (
+              <li key={index}>
+                  <img src={item.img} alt="item.img" />
+                  <h3>{item.name}</h3>
+                  <p className='p-info'>{item.info}</p>
+                  <p className='pr-info'>Rp{item.price}</p>
+                  <p className='ol__p-info'>{item.oldprice}</p>
+                  <span>{item.chegirma}</span>
+              </li>
+            ))
+          }
+        </ul>
+       </div>
     </div>
   )
 }
